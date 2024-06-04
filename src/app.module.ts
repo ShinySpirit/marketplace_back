ac64c81e58332162cd10a1d6dc386cd5deab3c0b
ShinySpirit <60615588+ShinySpirit@users.noreply.github.com>
2024-06-04 18:07:32 +0300

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { HealthcheckController } from './healthcheck/healthcheck.controller';
 
 @Module({
   imports: [
@@ -9,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
+  controllers: [AppController, HealthcheckController],
   providers: [AppService],
 })
 export class AppModule {}
